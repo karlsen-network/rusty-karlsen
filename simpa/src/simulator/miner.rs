b7042abc6ec64c7e8e4c200960708be5273570ab
Leon1777 <168653957+Leon1777@users.noreply.github.com>
2025-04-18 14:07:12 +0200

    create TN11 KIP10 HF activation and KIP9 beta switch

--- simpa/src/simulator/miner.rs
+++ simpa/src/simulator/miner.rs
@@ -6,7 +6,7 @@ use karlsen_consensus::params::Params;
 use karlsen_consensus_core::api::ConsensusApi;
 use karlsen_consensus_core::block::{Block, TemplateBuildMode, TemplateTransactionSelector};
 use karlsen_consensus_core::coinbase::MinerData;
-use karlsen_consensus_core::mass::{Kip9Version, MassCalculator};
+use karlsen_consensus_core::mass::MassCalculator;
 use karlsen_consensus_core::sign::sign;
 use karlsen_consensus_core::subnets::SUBNETWORK_ID_NATIVE;
 use karlsen_consensus_core::tx::{
@@ -157,7 +157,7 @@ impl Miner {
             .into_par_iter()
             .map(|mutable_tx| {
                 let signed_tx = sign(mutable_tx, schnorr_key);
-                let mass = self.mass_calculator.calc_tx_overall_mass(&signed_tx.as_verifiable(), None, Kip9Version::Alpha).unwrap();
+                let mass = self.mass_calculator.calc_tx_overall_mass(&signed_tx.as_verifiable(), None).unwrap();
                 signed_tx.tx.set_mass(mass);
                 let mut signed_tx = signed_tx.tx;
                 signed_tx.finalize();
