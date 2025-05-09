c6dcd0a6edca84a44774140960b0ecbc3ade7c89
Leon1777 <168653957+Leon1777@users.noreply.github.com>
2025-05-09 22:21:35 +0200

    some renaming KAS -> KLS

--- wallet/keys/src/derivation/gen1/mod.rs
+++ wallet/keys/src/derivation/gen1/mod.rs
@@ -1,4 +1,4 @@
-//! Derivation management for the Kaspa standard derivation scheme `'121337'`
+//! Derivation management for the Karlsen standard derivation scheme `'121337'`
 
 mod hd;
 pub use hd::{PubkeyDerivationManager, WalletDerivationManager};
